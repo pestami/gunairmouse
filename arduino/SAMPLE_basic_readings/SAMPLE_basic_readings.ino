ec00c15b8e0b5f1f0d33797837a6e2ddceb47c99
Mirek Pesta <mirekpesta@gmail.com>
2025-07-19 17:37:12 +0200

    ...

--- arduino/SAMPLE_basic_readings/SAMPLE_basic_readings.ino
+++ arduino/SAMPLE_basic_readings/SAMPLE_basic_readings.ino
@@ -6,38 +6,41 @@
 
 Adafruit_MPU6050 mpu;
 
-void setup(void) {
-  Serial.begin(115200);
+void setup(void) 
+{
+  Serial.begin(9600);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+      if (!mpu.begin()) 
+        {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1) {
+          delay(10);
+        }
   }
   Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  switch (mpu.getAccelerometerRange()) 
+      {
+      case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+      case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+      case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+      case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+      }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   Serial.print("Gyro range set to: ");
   switch (mpu.getGyroRange()) {
